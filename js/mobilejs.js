7e7e91c92cdf28afcaf4c8b33e3fffe6d85d200a
jhanter <templ4te@gmail.com>
2020-04-10 07:53:25 +0700

    commit
    perbaharuan undangn digital save the date

--- js/mobilejs.js
+++ js/mobilejs.js
@@ -50,5 +50,8 @@ function waorder(tpud){
 function waorderUdwebsite(tpudn){
 	window.location.href='https://api.whatsapp.com/send?phone=6281285869732&text=Saya%20Tertarik%20dengan%20undangan%20online%20website%20'+tpudn+'%20gimana%20cara%20ordernya?'
 }
+function waorderStd(std){
+	window.location.href='https://api.whatsapp.com/send?phone=6289605563525&text=Saya%20Tertarik%20dengan%20undangan%20Save%20The%20Date%20'+std+'%20gimana%20cara%20ordernya?'
+}
 
  
